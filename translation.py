1bba17526ec2bbd56b19c90a2124164bacc9ed7e
Jerin Johny <jerinjohny156@gmail.com>
2021-03-11 20:26:36 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,29 +1,28 @@
 class Translation(object):
-    START_TEXT = """ <b> Hi {} , I'm A Simple File Renamer+File To Video Converter Bot With Permanent Thumbnail support!💯</b> \n
+    START_TEXT = """ <b>Hi {}, I am a renamer bot with permanent thumbnail support. I can also convert files to video and download contents from Zee5.</b> \n\n
 
-<b>Bot Maintained By: @imjerin </b> \n\n
-<b>I Can Also Download/Upload Files From Zee5</b> \n\n
-<b>Do /help for more Details ...</b> \n
+<b><u><i>Send me any telegram file and reply /rename file name + extension for renaming.</i></u></b> \n\n
+<b><i>See /help for more</i></b>
 """
 
     RENAME_403_ERR = "What Are You Doing? Bruuh!!"
     BANNED_USER_TEXT = "Sorry!! But Mai Owner As Banned You From Using This Service. That Means You Can't Run Meh Now! \n \n Contact : @No_OnE_Kn0wS_Me For more Details.. " 
     ABS_TEXT = "What Are You Trying To Do,Mate?"
     UPGRADE_TEXT = "CONTACT @imjerin "
-    DOWNLOAD_START = "<b>Downloading To My server !! pls Wait</b>"
-    UPLOAD_START = "<b>Downloading Completed Now I'm Uploading Into TG</b>"
+    DOWNLOAD_START = "<i><b>DownloadiNg and Renaming⛕ Please Wait!!</b></i>"
+    UPLOAD_START = "<i><b>Uploading⛕ Please Wait!!🕺</b></i>"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry Do You Think! I'll Upload It?"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me SHARE > ❤️**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "<u><b>Thank you for Using Me ❤️</b></u>"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds"
     NOT_AUTH_USER_TEXT = "CONTACT @imjerin "
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "IF You Got This Message You Should Contact @imjerin "
-    SAVED_CUSTOM_THUMB_NAIL = "<b>thumbnail Saved ✅ This Is Permanent Until</b> /delthumb ❤"
-    DEL_ETED_CUSTOM_THUMB_NAIL = "thumbnail cleared succesfully🤦"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "If You Got This Message You Should Contact @imjerin "
+    SAVED_CUSTOM_THUMB_NAIL = "<b>Thumbnail Saved ✅ This is Permanent Until</b> /delthumb ❤"
+    DEL_ETED_CUSTOM_THUMB_NAIL = "Thumbnail cleared succesfully🤦"
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "Media cleared succesfully."
     SAVED_RECVD_DOC_FILE = "<b>File Downloaded Successfully 😎</b>"
-    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To An File With /rename fil name extension To rename a file</b>"
-    REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To An File With /c2v To Convert It Into Streamable video File</b>"
-    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To An File/video With /c2a To Convert It Into Audio File</b>"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "<b>Please Reply To a File With /rename file name + extension to rename a file</b>"
+    REPLY_TO_DOC_FOR_C2V = "<b> Please Reply To a File With /c2v To Convert It Into Streamable video File</b>"
+    REPLY_TO_DOC_FOR_C2A = "<b> Please Reply To a File/video with /c2a To Convert It Into Audio File</b>"
     CUSTOM_CAPTION_UL_FILE = " "
     NO_THUMB_FOUND = "No Thumbnail found"
     FORMAT_SELECTION = """<b>Choose appropriate option</b> <a href='{}'>⬇️</a>
@@ -33,12 +32,22 @@ class Translation(object):
 """
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
     IFLONG_FILE_NAME = """You Gotta Be Kidding Me...Decrease The Number Of Letters😆😉"""
-    ABOUT_ME = """<b>Meh : \n An Telegram File Renamer Bot Which Can Rename A Telegram Files. \n You Can Set Permanent Thumbnail For The File So You Don't Have To Send Custom Thumbs All The Time.\n \n i can also convert files into videos \n Feedback @imjerin If You Found Meh Useful</b>"""
-    HELP_USER = """Ohh You Want Help?? 😅
+    HELP_USER = """<u><b>Instructions</b></u>
     
-1.🔹 <b>Send me any Telegram File.</b> \n
-2.🔹 <b>Reply to that message with /rename New file name.extnsion</b> \n
-3.🔹 <b> Send A Photo to make it as permanent Thumbnail</b> \n
-4.🔹 <b> I Can Also Convert Files Into Video through /c2v Command </b>   \n
-5.🔹 <b> Send Me Any Zee5 Url And I'll Upload It Into Telegram </b> \n \n <b>Made With Love By: @imjerin </b>"""
+1.🔹 <i><b>Send me any Telegram File.</b></i> \n
+2.🔹 <i><b>Reply to that message with /rename New file name.extnsion</b></i> \n
+3.🔹 <i><b>Send A Photo to make it as permanent Thumbnail</b></i> \n
+4.🔹 <i><b>I Can Also Convert Files Into Video through /c2v Command</b></i>   \n
+5.🔹 <i><b>Send Me Any Zee5 Url And I'll Upload It Into Telegram</b></i> \n \n <b>Maintained by @imjerin</b>"""
+
+
+    ABOUT_ME = """<u><b>BOT DETAILS:</b></u>
+
+🤖 <b>Bot Name:</b> <a href='https://t.me/klrenameitbot'>Renamer</a>
+    
+📝 <b>Language:</b> <a href='https://python.org'>Python 3</a>
+
+🧰 <b>Framework:</b> <a href='https://github.com/jerinjohny-ktnm/pyrogram'>Pyrogram</a>
+
+👤 <b>Developer:</b> <a href='https://t.me/imjerin'>Jerin</a>"""
 
